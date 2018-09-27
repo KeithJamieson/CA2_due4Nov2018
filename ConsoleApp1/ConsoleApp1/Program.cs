543f349372d714d6649a4fb987ae01e8f48e316e
Keith Jamieson <l00150011@student.lyit.ie>
2018-09-27 21:23:51 +0100

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 1;
+            int secondNumber = 5;
+            int result = firstNumber + secondNumber;
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {result} ");
+            Console.ReadLine();
+        }
+    }
+}
