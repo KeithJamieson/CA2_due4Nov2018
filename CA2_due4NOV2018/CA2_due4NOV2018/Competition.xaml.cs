607fbab23db65065f1153ab15092cb2cbc7ef73f
Keith Jamieson <l00150011@student.lyit.ie>
2018-10-27 19:50:07 +0100

    CA2  Scrren Designs Register, Logon Dashboard_secretary Dashboard_Member , CompetitionViewEntries

---
+++ CA2_due4NOV2018/CA2_due4NOV2018/Competition.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CA2_due4NOV2018
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddRiderEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAssignFinishingPositions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCloseCompetition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdateLeaderBoard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+    }
+}
---
+++ CA2_due4NOV2018/CA2_due4NOV2018/Dashboard.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CA2_due4NOV2018
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeMyPassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnModifyMyDetails_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnModifyMemberDetails_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnViewLeaderboard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    
+
+    private void btnViewReports_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnListNewMembers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnViewScheduledCompetitions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddClub_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpenCompetition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
